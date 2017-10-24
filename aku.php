4b9801181bcbe71fdc120765efeeba6d334abec9
unknown <171610132@smkassalaambandung.sch.id>
2017-10-24 09:06:35 +0700

    tugas ketiga saya

---
+++ aku.php
@@ -0,0 +1,12 @@
+<?php
+class Putri_duyung{
+
+ 	//property
+ 	public $badan = 'lucu';
+ 	public $wajah = 'lumayan cantik';
+
+ }
+ $Putri_duyung1 = new Putri_duyung;
+ echo "Putri duyung itu memiliki badan yang " .$Putri_duyung1-> badan.'<br>';
+ echo "Putri duyung itu memiliki wajah yang " .$Putri_duyung1-> wajah.'<br>';
+ ?> 
---
+++ harimau.php
@@ -0,0 +1,11 @@
+<?php
+class harimau{
+
+ 	//property
+ 	public $wajah ='seram';
+ 	public $sikap ='galak';
+ }
+ 	$harimau1 = new harimau;
+ echo "harimau itu memiliki wajah yang " .$harimau1-> wajah.'<br>';
+ echo "harimau itu memiliki wajah yang " .$harimau1-> sikap. '<br>';
+ ?>
